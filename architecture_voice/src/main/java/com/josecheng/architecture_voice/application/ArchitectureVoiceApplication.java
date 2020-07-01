2a4a58e5cc3ad847c72193ac29eb8134e0552b94
jose <josecheng92@163.com>
2020-07-01 22:47:27 +0800

    add replugin

--- architecture_voice/src/main/java/com/josecheng/architecture_voice/application/ArchitectureVoiceApplication.java
+++ architecture_voice/src/main/java/com/josecheng/architecture_voice/application/ArchitectureVoiceApplication.java
@@ -1,14 +1,15 @@
 package com.josecheng.architecture_voice.application;
 
-import android.app.Application;
-
 import com.alibaba.android.arouter.launcher.ARouter;
 import com.josecheng.architecture_video.app.VideoHelper;
 import com.josecheng.lib_audio.app.AudioHelper;
 import com.josecheng.lib_share.share.ShareManager;
+import com.qihoo360.replugin.RePluginApplication;
 
-public class ArchitectureVoiceApplication extends Application {
+public class ArchitectureVoiceApplication extends RePluginApplication {
+
     private static ArchitectureVoiceApplication architectureVoiceApplication = null;
+
     @Override
     public void onCreate() {
         super.onCreate();
